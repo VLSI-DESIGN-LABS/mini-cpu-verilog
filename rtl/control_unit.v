705d51fa49e1a18ba7c20b9a77c819f2d64f4fcc
Manvith.M <manvith.m.7204@gmail.com>
2026-02-21 23:23:41 +0530

    Implement control unit module with ALU opcode logic

--- rtl/control_unit.v
+++ rtl/control_unit.v
@@ -1,1 +1,26 @@
+module control_unit(
+    input [15:0] instruction,
 
+    output reg [3:0] alu_opcode,
+    output reg [2:0] read_reg1,
+    output reg [2:0] read_reg2,
+    output reg [2:0] write_reg,
+    output reg write_enable
+);
+
+always @(*) begin
+    alu_opcode = instruction[15:12];
+    write_reg  = instruction[11:9];
+    read_reg1  = instruction[8:6];
+    read_reg2  = instruction[5:3];
+   
+    write_enable = 1;
+
+    case(alu_opcode)
+        4'b1001: write_enable = 0; 
+        default: write_enable = 1;
+    endcase
+
+end
+
+endmodule
